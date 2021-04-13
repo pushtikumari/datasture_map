8e84e567934a6d9a3143b81ca943028016daee2d
pushtikumari <66685092+pushtikumari@users.noreply.github.com>
2021-04-13 20:25:17 +0530

    final

---
+++ maps_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	. "maps"
+	"fmt"
+)
+
+
+//---
+type Person struct {
+	Name    string
+	Surname string
+	Age     int
+}
+
+func (p *Person) Key() interface{} {
+	return p.Name + "#" + p.Surname
+}
+
+//---
+
+
+func main() {
+
+	p1 := &Person{"John", "Rambo", 60}
+	p2 := &Person{"John", "Doe", 30}
+	p3 := &Person{"Jane", "Doe", 30}
+
+	//Create map:
+	M := make(Map)
+
+	//Insert into map:
+	M.Insert(p1, "Manager")
+	M.Insert(p2, "Employee")
+	M.Insert(p3, "Secretary")
+
+	//Action to perform during iteration:
+	printValues := func(k interface{}, v interface{}) {
+		fmt.Printf("Person, Job = %v, %s\n", k, v)
+	}
+
+	//Iterate over Map:
+	M.Do(printValues)
+
+	println("---")
+
+	//Query map:
+	p := &Person{"John", "Rambo", 60}
+	Value, Ok := M.Get(p)
+	if Ok {
+		fmt.Printf("Job = %s\n", Value)
+	}
+
+	println("---")
+
+	//Delete from map
+	M.Delete(p1)
+
+	//Check the result of the deletion,
+	//second iteration over map 
+	//(using a function litteral, as an alternative):
+	M.Do(func(k interface{}, v interface{}) {
+		fmt.Printf("Person, Job = %v, %s\n", k, v)
+	})
+
+	fmt.Printf("Size of map M = %d\n", M.Len())
+
+}
